fa3ac6f104632d4aeddc975c7bfcb91243cdf1f2
JoseHub <josemari982010@gmail.com>
2018-06-04 18:29:58 +0200

    Add files via upload

---
+++ IIS-ProyectoGrupo1/src/Examen.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Examen {
+	
+	public Examen() {
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Date getFecha() {
+		return new Date(5, 5, 5);
+	}
+	
+}
