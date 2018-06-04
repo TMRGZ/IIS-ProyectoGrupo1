9c668b8a10d5bd57bf974b519164b559fc92dfbd
josema <josema@jose.laptop>
2018-06-04 16:40:14 +0200

    Asignatura v1

---
+++ src/TramaHoraria.java
@@ -0,0 +1,14 @@
+
+public class TramaHoraria {
+	private double HoraInicio;
+	private int Dia;
+	
+	public TramaHoraria(double HoraInicio, int Dia) {
+		this.HoraInicio = HoraInicio;
+		this.Dia = Dia;
+	}
+	public double getHoraInicio() {
+		return HoraInicio;
+	}
+	
+}
